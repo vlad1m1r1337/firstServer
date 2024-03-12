66839714a781d02749ce48e3ebf4e9d58d8b2578
Vlad1m1r1337 <vladim1r.gribkov@yandex.ru>
2024-03-12 09:34:07 +0400

    finish

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const getUsers = require('./modules/users');
+const url = require('url');
+const { URLSearchParams } = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const searchParams = new URLSearchParams(parsedUrl.query);
+    console.log(searchParams.get('hello'));
+
+    if (searchParams.get('hello') !== null) {
+        if (parsedUrl.query.hello) {
+            res.statusCode = 200;
+            res.statusMessage = "OK";
+            res.header = "Content-Type: text/plain";
+            res.write("Hello " + parsedUrl.query.hello);
+            res.end();
+            return;
+        }
+        else {
+            res.statusCode = 400;
+            res.statusMessage = "Bad Request";
+            res.header = "Content-Type: text/plain";
+            res.write("Enter a name");
+            res.end();
+            return;
+
+        }
+    }
+    if ('users' in parsedUrl.query) {
+        res.statusCode = 200;
+        res.statusMessage = "OK";
+        res.header = "Content-Type: application/json";
+        res.write(getUsers());
+        res.end();
+
+        return;
+    }
+    res.statusCode = 200;
+    res.statusMessage = "OK";
+    res.header = "Content-Type: text/plain";
+    res.write("Hello, World!");
+    res.end();
+})
+
+server.listen(3003, () => console.log("Сервер запущен по адресу http://localhost:3003/"));
---
+++ src/modules/users.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const getUsers = () => {
+    const filePath = path.join(__dirname, "../data/users.json");
+    return fs.readFileSync(filePath);
+}
+
+module.exports = getUsers;
